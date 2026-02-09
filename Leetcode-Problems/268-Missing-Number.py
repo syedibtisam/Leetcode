2dc52f553dfa04b7d4653312dd2b0794a3ee360b
syedibtisam <m.ibtisam.syed@gmail.com>
2026-02-09 17:28:18 +0000

    Problem 268. Missing Number: Sovled

---
+++ Leetcode-Problems/268-Missing-Number.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        unique_values = set()
+        for i in range(len(nums) + 1):
+            unique_values.add(i)
+        
+        for value in nums:
+            unique_values.remove(value)
+        
+        return unique_values.pop()
